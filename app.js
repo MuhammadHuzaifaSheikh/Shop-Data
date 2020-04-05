c80aef4a51798f8b1dea0bc5b482cae667a4774a
Muhammad Huzaifa Shaikh <huzaifa900shaikh@gmail.com>
2020-04-05 21:23:21 +0500

    Second Commit

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ var buy = document.getElementById('buy');
 var sell = document.getElementById('sell');
 var quntity = document.getElementById('quntity');
 var search = document.getElementById('search');
-
+var savebtn = document.getElementById('save');
 var db = firebase.firestore();
+
+
+var totalQuantity;
+
+
+
+var isUpdate;
+var updatedId;
 
 function saveData() {
 
@@ -14,21 +22,43 @@ function saveData() {
         buy: buy.value,
         sell: sell.value,
         quntity: quntity.value,
+        income : sell.value-buy.value
 
     };
 
-    db.collection("shop").add(productDetails)
-        .then(function (docRef) {
-            console.log("Document written with ID: ", docRef.id);
-            product.value = "";
-            buy.value = "";
-            sell.value = "";
-            quntity.value = 0;
-        })
-        .catch(function (error) {
-            console.error("Error adding document: ", error);
-        });
 
+    if (isUpdate) {
+        db.collection("shop").doc(updatedId).update({
+            product: product.value,
+            buy: buy.value,
+            sell: sell.value,
+            quntity: quntity.value,
+            income : sell.value-buy.value,
+            id : updatedId,
+        });
+        savebtn.innerHTML = 'Save';
+        isUpdate = false;
+        product.value = '';
+        buy.value = '';
+        sell.value = '';
+        quntity.value = '';
+
+    }
+
+
+    else {
+        db.collection("shop").add(productDetails)
+            .then(function (docRef) {
+                console.log("Document written with ID: ", docRef.id);
+                product.value = "";
+                buy.value = "";
+                sell.value = "";
+                quntity.value = 0;
+            })
+            .catch(function (error) {
+                console.error("Error adding document: ", error);
+            });
+    }
 
 }
 
@@ -40,12 +70,23 @@ function list() {
                 u.id = change.doc.id;
                 console.log(u);
                 console.log(change);
+
+                totalQuantity=u.quntity;
+                console.log(totalQuantity);
                 if (change.type === "added") {
                     rander(u);
                     console.log("New city: ", change.doc.data());
                 }
                 if (change.type === "modified") {
                     console.log("Modified city: ", change.doc.data());
+                    var td1 = document.getElementById(u.id).firstElementChild;
+                    var td2 = td1.nextSibling;
+                    var td3 = td2.nextSibling;
+                    var td4 = td3.nextSibling;
+                    td1.innerHTML =u.product;
+                    td2.innerHTML = u.buy;
+                    td3.innerHTML =u.sell;
+                    td4.innerHTML = u.quntity
 
 
                 }
@@ -69,24 +110,51 @@ function rander(u) {
     var w3 = document.getElementById("th3");
     var w4 = document.getElementById("th4");
     var w5 = document.getElementById("th5");
+    var w6 = document.getElementById("th6");
 
     var z1 = document.createElement("TD");
     var z2 = document.createElement("TD");
     var z3 = document.createElement("TD");
     var z4 = document.createElement("TD");
     var z5 = document.createElement("TD");
+    var z6 = document.createElement("TD");
 
 
     w1.innerHTML = 'Product Name';
     w2.innerHTML = 'Buy Prize';
     w3.innerHTML = 'Sell Prize';
     w4.innerHTML = 'Quantity';
+    w5.innerHTML = 'Income';
+    w6.innerHTML = 'Edit';
 
 
     z1.innerHTML = u.product;
     z2.innerHTML = u.buy;
     z3.innerHTML = u.sell;
     z4.innerHTML = u.quntity;
+    z5.innerHTML = 0;
+
+    var editButton = document.createElement("button");
+
+    editButton.innerHTML = 'Edit';
+
+    z6.appendChild(editButton);
+
+
+
+
+
+    z6.addEventListener('click', function () {
+        product.value = u.product;
+        buy.value = u.buy;
+        sell.value = u.sell;
+        quntity.value = u.quntity;
+
+        isUpdate = true;
+        updatedId = u.id;
+
+        savebtn.innerHTML = 'Update';
+    });
 
 
 
@@ -95,6 +163,7 @@ function rander(u) {
     y.appendChild(z3);
     y.appendChild(z4);
     y.appendChild(z5);
+    y.appendChild(z6);
 
     x.appendChild(y);
     document.getElementById("myTable").appendChild(x);
@@ -103,11 +172,14 @@ function rander(u) {
 
 function searchByProduct(n) {
 
+
     db.collection("shop").where("product", "==", search.value)
         .get()
         .then(function (querySnapshot) {
             querySnapshot.forEach(function (doc) {
                 console.log(doc.id, " => ", doc.data());
+
+                console.log(doc.data());
 
                 var thh1 = document.getElementById('thh1');
                 var thh2 = document.getElementById('thh2');
@@ -133,22 +205,51 @@ function searchByProduct(n) {
                 td2.innerHTML = doc.data().buy;
                 td3.innerHTML = doc.data().sell;
                 td4.innerHTML = q;
+                td5.innerHTML = 0;
 
                 search.value = '';
 
                 var soldButton = document.getElementById('button');
 
                 soldButton.innerHTML = 'Sold';
+                var income = doc.data().income;
 
                 soldButton.addEventListener('click', function () {
-                    td4.innerHTML = --q;
 
+                    if (q > 0){
+                        td4.innerHTML = --q;
 
-                    if (q === 0) {
-                        alert('Please Insert Some Product');
                     }
 
-                })
+                else{
+                        alert('Please Insert Some Product')
+                    }
+
+                    console.log(totalQuantity);
+                    td5.innerHTML=(totalQuantity- td4.innerHTML)*income;
+
+
+                    var updatebtn2= document.getElementById('updatebtn2');
+                        updatebtn2.innerHTML='OK/Save';
+                    updatebtn2.addEventListener('click',function () {
+
+                        db.collection("shop").doc().update({
+
+
+                            quntity: td4.innerHTML,
+                            income : td5.innerHTML,
+
+
+
+                        });
+                        console.log(doc.data());
+
+                    })
+                });
+
+
+
+
 
 
             });
@@ -156,4 +257,7 @@ function searchByProduct(n) {
         .catch(function (error) {
             console.log("Error getting documents: ", error);
         });
+
+
+
 }
